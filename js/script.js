1188ab7d8f23b0b0a586c7771dc7d09af609d7d5
wulanhrl <wulanherlina0108@gmail.com>
2025-07-08 00:12:37 +0700

    Update js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   products.forEach(product => {
     const simpanBtn = document.createElement("button");
-    simpanBtn.textContent = "💙 Simpan";
+    simpanBtn.textContent = "❤ Simpan";
     simpanBtn.classList.add("wishlist-btn");
 
     simpanBtn.addEventListener("click", () => {
